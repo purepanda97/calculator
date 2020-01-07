835068bfc3bcadbef054c0708125ad1b4c15fdc7
purepanda97 <purepanda97@gmail.com>
2020-01-07 14:26:35 +0900

    main

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <studio.h>
+
+int add(int, int);
+int sub(int, int);
+int div(int, int);
+int multi(int, int);
+
+int main(int argc, char* argv[])
+{
+    int a, b, c;
+    char operator;
+
+    printf("Please enter a expression");
+    scanf("%d %c %d", &a, &operator, &b);
+
+    switch(operator)
+    {
+        case '+':
+        c = add(a, b);
+        break;
+        
+        case '-':
+        c = sub(a, b);
+        break;
+
+        case '*';
+        c = multi(a, b);
+        break;
+
+        case '/';
+        c = div(a, b);
+        break;
+    }
+}
